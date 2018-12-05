f4313737c41172f58ded44539d385865a06bd34a
stranger7771981 <denis_uglov@mail>
2018-12-05 16:25:19 +0600

    Add akipress crawler ver1

---
+++ src/example/Aki_headers.py
@@ -0,0 +1,31 @@
+def extractdata(context, data):
+    # This stage comes after 'fetch' so the 'data' input contains an
+    # HTTPResponse object.
+    response = context.http.rehash(data)
+    url = response.url
+    page = response.html
+
+    # Parse the rest of the page to extract structured data.
+
+    header = _gettext(page.xpath('.//h2[@class='title''))
+    #autor = _gettext(page.xpath('.//header/div/div/a/text()'))
+    picture = _gettext(page.xpath('(.//@src')))  
+	
+    article_data = {
+        "url": response.url,
+        "header": header,
+		#"autor": autor,
+		"picture": picture
+    }
+
+
+    if article_data["header"] is not None:
+        # If 'rule' is not set, it defaults to 'pass', which triggers the
+        # final 'store' stage.
+        context.emit(data=article_data)
+		
+def _gettext(list):
+    if not list:
+        return list
+    else:
+        return list[0].strip()
